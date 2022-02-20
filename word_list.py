adc3ef45578252e2fa7edbed3cd5ac9e28167b81
Ahraz Amid Islam <91738212+ahrazamid@users.noreply.github.com>
2022-02-20 21:51:44 +0600

    Update word_list.py

--- word_list.py
+++ word_list.py
@@ -1,10 +1,6 @@
 
-
-
+### word list
+### enter words in the list
+### might cause errors if words are longer than 5 letters
+### to add more than 6 letters modify the range in the nested for loop
 words = ['hello','shark','ahraz']
-
-
-tries = 0
-guess = ""
-indi_letters = []
-indi_wotd = []
